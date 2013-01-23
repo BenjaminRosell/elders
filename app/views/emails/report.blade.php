9456a3c4ce5b727ffb0608cbb4e305418c8100d3
Benjamin Gonzalez <benjamin.rosell@gmail.com>
2013-01-23 10:34:03 -0500

    Editing Homepage

--- app/views/emails/report.blade.php
+++ app/views/emails/report.blade.php
@@ -1,7 +1,8 @@
-<h1>Hello !</h1>
-
-<p>Another month is now past and we are eager to know how your families are doing.</p>
-
-<p>Please help us be better prepared for our priesthood interview by completing the home teaching report available by clicking the following link:</p>
-
-<a href="http://local.elders.com/visits">Home teaching teports</a>
+<html>
+	<body>
+		<h1>Hello !</h1>
+		<p>Another month is now past and we are eager to know how your families are doing.</p>
+		<p>Please help us be better prepared for our priesthood interview by completing the home teaching report available by clicking the following link:</p>
+		<a href="http://local.elders.com/visits">Home teaching teports</a>
+	</body>
+</html>
