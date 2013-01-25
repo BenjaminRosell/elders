3853c94d18768bd2c0765cad7fc213ec59e88910
Benjamin Gonzalez <benjamin.rosell@gmail.com>
2013-01-25 14:30:58 -0500

    Preparing Pagodabox deploy

--- app/config/database.php
+++ app/config/database.php
@@ -57,10 +57,9 @@ return array(
 			'host'     => 'localhost',
 			'database' => 'laravel',
 			'username' => 'root',
-			'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
 			'password' => 'root',
 			'charset'  => 'utf8',
-			'collation' => 'utf8_general_ci',
+			'collation'=> 'utf8_general_ci',
 			'prefix'   => '',
 		),
 
---
+++ app/config/local/database.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'   => 'mysql',
+			'host'     => 'localhost',
+			'database' => 'laravel',
+			'username' => 'root',
+			'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
+			'password' => 'root',
+			'charset'  => 'utf8',
+			'collation' => 'utf8_general_ci',
+			'prefix'   => '',
+		),
+
+	),
+
+);
