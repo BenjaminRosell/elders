e878fe327a30a880ee300a364a447cfba7019657
Benjamin Gonzalez <benjamin.rosell@gmail.com>
2013-01-26 05:17:36 -0500

    Fixing environments and database

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -2,21 +2,17 @@
 
 return array(
 
-
 	'connections' => array(
-
 		'mysql' => array(
-			'driver'   => 'mysql',
-			'host'     => 'localhost',
-			'database' => 'laravel',
-			'username' => 'root',
+			'driver'   	=> 'mysql',
+			'host'     	=> 'localhost',
+			'database' 	=> 'laravel',
+			'username' 	=> 'root',
 			'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
-			'password' => 'root',
-			'charset'  => 'utf8',
+			'password' 	=> 'root',
+			'charset'  	=> 'utf8',
 			'collation' => 'utf8_general_ci',
-			'prefix'   => '',
-		),
-
-	),
-
+			'prefix'   	=> '',
+		)
+	)
 );
--- start.php
+++ start.php
@@ -41,7 +41,8 @@ $app->instance('path.base', __DIR__);
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'local' => array('local.elders.dev'),
+	'production' => array('*.com'),
 
 ));
 
